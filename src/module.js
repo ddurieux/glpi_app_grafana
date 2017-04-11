6132d80bfe733dbf50d9e8109278253a6f8e3f67
David Durieux <d.durieux@siprossii.com>
2017-04-11 10:02:17 +0200

    Create structure of folder

---
+++ src/module.js
@@ -0,0 +1,9 @@
+import {LogsPageCtrl} from './components/logs';
+import {StreamPageCtrl} from './components/stream';
+import {GLPIAppConfigCtrl} from './components/config';
+
+export {
+  GLPIAppConfigCtrl as ConfigCtrl,
+  StreamPageCtrl, //Matches pages.component in plugin.json
+  LogsPageCtrl //Matches pages.component in plugin.json
+};
