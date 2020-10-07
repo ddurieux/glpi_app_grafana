1ea73147aef8ffb43cbbd18a848d0cfb6240e640
David Durieux <david@durieux.family>
2020-10-07 10:37:16 +0200

    (many things done...)

--- src/datasource/module.ts
+++ src/datasource/module.ts
@@ -1,16 +1,15 @@
-import * as moment from "../vendor/public/builds/moment-timezone-with-data";
-import {GlpiAppDatasource} from "./datasource";
-import {GlpiAppDatasourceQueryCtrl} from "./query_ctrl";
+import * as moment from '../vendor/public/builds/moment-timezone-with-data';
+import { GlpiAppDatasource } from './datasource';
+import { GlpiAppDatasourceQueryCtrl } from './query_ctrl';
 
 export class GlpiAppDatasourceConfigCtrl {
   public timezoneList = moment.tz.names();
-  static templateUrl = "datasource/partials/config.html";
+  static templateUrl = 'datasource/partials/config.html';
   current: any;
 
   constructor($scope) {
-    this.current.jsonData.timezone = this.current.jsonData.timezone || "UTC";
+    this.current.jsonData.timezone = this.current.jsonData.timezone || 'UTC';
   }
-
 }
 
 export {
